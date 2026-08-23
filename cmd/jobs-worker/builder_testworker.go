cbdb80832f3aa5212a5dc4f51ed607a3cfc96126
Daniil Koryto <daniil.koryto@gmail.com>
2026-08-23 21:52:27 +0300

    fix: harden River jobs lifecycle and retries

--- cmd/jobs-worker/builder_testworker.go
+++ cmd/jobs-worker/builder_testworker.go
@@ -23,12 +23,19 @@ type jobsWorkerTestWorker struct {
 	river.WorkerDefaults[jobsWorkerTestArgs]
 }
 
-func (*jobsWorkerTestWorker) Work(_ context.Context, job *river.Job[jobsWorkerTestArgs]) error {
+func (*jobsWorkerTestWorker) Work(ctx context.Context, job *river.Job[jobsWorkerTestArgs]) error {
 	path := os.Getenv("JOBS_WORKER_TEST_MARKER")
 	if path == "" {
 		return errors.New("JOBS_WORKER_TEST_MARKER is required")
 	}
-	return os.WriteFile(path, []byte(job.Args.Value), 0o600)
+	if err := os.WriteFile(path, []byte(job.Args.Value), 0o600); err != nil {
+		return err
+	}
+	if job.Args.Value == "wait-for-cancel" {
+		<-ctx.Done()
+		return ctx.Err()
+	}
+	return nil
 }
 
 var buildWorkers bootstrap.WorkersBuilder = func(
